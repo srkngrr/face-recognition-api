8953168eac442c201ad8f6acb2ae0270b308fea3
Serkan <serkangurer@yahoo.com>
2020-03-20 16:33:25 +0300

    deploying project to Git-Hub

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser') // Middleware for reading JSON files.
+const cors = require('cors'); // for working in localhost in safely
+const knex = require('knex'); // connecting server to database
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'Gurer',
+    password : '',
+    database : 'face-recognition'
+  }
+});
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req,res) => db.select('*').from('users').then(user => res.json(user)))
+app.post('/signin', (req,res) => {signin.handleSignIn(req, res, db, bcrypt)})
+app.post('/register', (req,res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req, res, db)})
+app.put('/image', (req,res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)})
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000')
+})
