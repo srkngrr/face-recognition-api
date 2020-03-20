47a946753a4a7cc3ac571cc7f295adfdeebda9ee
Serkan <serkangurer@yahoo.com>
2020-03-20 17:49:50 +0300

    updating database adress

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-rigid-63630',
     user : 'Gurer',
     password : '',
     database : 'face-recognition'
