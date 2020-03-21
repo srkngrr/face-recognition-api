2e45f9bf51b0c94a4ff4a5a239fc8c1e40e3bfa3
Serkan <serkangurer@yahoo.com>
2020-03-21 12:15:47 +0300

    fix

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,5 @@
-
 const handleRegister = (req, res, db, bcrypt) => {
+	const saltRounds = 10;
 	const { email, name, password } = req.body
 	if(!email || !name || !password) {
 		return res.status(400).json('incorrect form submission')
